a72770240db855c8050ce85c27152ab46d0d5c1b
Xufeng Shen <xufeng_shen@toyota.ca>
2022-06-26 13:44:33 -0400

    Initial Commit

---
+++ TestApp3/TestApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp3App.swift
+//  TestApp3
+//
+//  Created by user222240 on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
