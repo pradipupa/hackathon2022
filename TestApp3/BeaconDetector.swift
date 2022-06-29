8157c5e5074573058cc0c992b89ff37a787bfdd6
Xufeng Shen <xufeng_shen@toyota.ca>
2022-06-28 22:21:05 -0400

    Add Beacon detector

---
+++ TestApp3/BeaconDetector.swift
@@ -0,0 +1,59 @@
+//
+//  BeaconDetector.swift
+//  TestApp3
+//
+//  Created by user222240 on 6/28/22.
+//
+
+import Foundation
+import CoreLocation
+import SwiftUI
+
+class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate  {
+    var locationManager: CLLocationManager?
+    @Published var lastDistance = CLProximity.unknown
+    
+    override init() {
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()        
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // we can take action here
+                    startScanning()
+                }
+            }
+        }
+    }
+       
+    func startScanning() {
+        //hardocde for now
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 123, minor: 456)
+        
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "MyBeacon")
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: constraint)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        if let beacon = beacons.first {
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
+    
+    
+    func update(distance: CLProximity) {
+        lastDistance = distance
+    }
+    
+}
